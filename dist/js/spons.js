77b7a5a806f5001c609d1afbc1ca34f75a6fdd0e
Chinmay Pandhare <pandharechinmay882@gmail.com>
2017-10-26 06:06:18 +0530

    Sponsors logos

--- dist/js/spons.js
+++ dist/js/spons.js
@@ -2,111 +2,134 @@ var spons = [
     {
         name: "OnePlus",
         title: "Title Sponsor",
-        logo: "spons/oneplus.svg"
+        logo: "img/spons/oneplus.svg"
+    },
+    {
+        name: "VH1",
+        title: "Associate Title Sponsor",
+        logo: "img/spons/vh1.png"
+    },
+    {
+        name: "Comedy Central",
+        title: "Associate Title Sponsor",
+        logo: "img/spons/comedy-central.jpg"
     },
     {
         name: "Marshall",
         title: "Powered By",
-        logo: "spons/marshall.jpg"
+        logo: "img/spons/marshall.jpg"
     },
     {
-        name: "Hewlett-Packard",
-        title: "Technology Partner",
-        logo: "spons/hp.png"
+        name: "Droom",
+        title: "N2O Powered By",
+        logo: "img/spons/droom.jpg"
     },
     {
         name: "Living Talent",
         title: "Talent Partner",
-        logo: "spons/living-talent.png"
+        logo: "img/spons/living-talent.png"
     },
     {
-        name: "Comedy Central",
-        title: "In Association With",
-        logo: "spons/comedy-central.jpg"
+        name: "Honda",
+        title: "Riding Partner",
+        logo: "img/spons/honda.jpg"
+    },
+    {
+        name: "Hewlett-Packard",
+        title: "Technology Partner",
+        logo: "img/spons/hp.png"
     },
     {
         name: "ISIC",
-        title: "Gold Sponsor",
-        logo: "spons/isic.jpg"
-    },
-    {
-        name: "ATKT",
-        title: "Media Partner",
-        logo: "spons/atkt.png"
-    },
-    {
-        name: "The Week",
-        title: "Media Partner",
-        logo: "spons/the-week.png"
-    },
-    {
-        name: "Whack",
-        title: "Media Partner",
-        logo: "spons/whack.png"
+        title: "Student Partner",
+        logo: "img/spons/isic.jpg"
     },
     {
         name: "Embassy of Israel",
         title: "International Partner",
-        logo: "spons/israel-embassy.png"
+        logo: "img/spons/israel-embassy.png"
+    },
+    {
+        name: "Embassy of Israel",
+        title: "International Partner",
+        logo: "img/spons/israel-2.png"
     },
     {
         name: "Embassy of Portugal",
         title: "International Partner",
-        logo: "spons/portugal-embassy.png"
+        logo: "img/spons/portugal-embassy.png"
+    },
+    {
+        name: "Whack",
+        title: "YouTube Partner",
+        logo: "img/spons/whack.png"
     },
     {
         name: "Saavn",
         title: "Streaming Partner",
-        logo: "spons/saavn.png"
-    },
-    {
-        name: "InShorts",
-        title: "Media Partner",
-        logo: "spons/inshorts.png"
+        logo: "img/spons/saavn.png"
     },
     {
         name: "Paisa Wapas",
         title: "Cashback Partner",
-        logo: "spons/paisa-wapas.png"
-    },
-    {
-        name: "Student Stories",
-        title: "Media Partner",
-        logo: "spons/student-stories.png"
-    },
-    {
-        name: "Apollo Munich Insurance",
-        title: "Platinum Sponsor",
-        logo: "spons/apollo-munich.jpg"
-    },
-    {
-        name: "WooHoo",
-        title: "Standup Soapbox Partner",
-        logo: "spons/woohoo.svg"
-    },
-    {
-        name: "9xO",
-        title: "Media Partner",
-        logo: "spons/9xo.png"
-    },
-    {
-        name: "Droom",
-        title: "N2O Partner",
-        logo: "spons/droom.jpg"
+        logo: "img/spons/paisa-wapas.png"
     },
     {
         name: "The Incredible Photographers",
         title: "Photography Partner",
-        logo: "spons/the-incredible-photographers.jpg"
+        logo: "img/spons/the-incredible-photographers.jpg"
+    },
+    {
+        name: "Furtados",
+        title: "Instrument Partner",
+        logo: "img/spons/furtados.jpg"
+    },
+    {
+        name: "WooHoo",
+        title: "Standup Soapbox Partner",
+        logo: "img/spons/woohoo.svg"
     },
     {
         name: "Anahad Foundation",
         title: "Tarang Associate Partner",
-        logo: "spons/anahad.png"
+        logo: "img/spons/anahad.png"
     }
 ];
 
-for (var s in spons) {
+var medias = [
+  {
+      name: "ATKT",
+      title: "Media Partner",
+      logo: "img/spons/atkt.png"
+  },
+  {
+      name: "9xO",
+      title: "Media Partner",
+      logo: "img/spons/9xo.png"
+  },
+  {
+      name: "InShorts",
+      title: "Media Partner",
+      logo: "img/spons/inshorts.png"
+  },
+  {
+      name: "Student Stories",
+      title: "Media Partner",
+      logo: "img/spons/student-stories.png"
+  },
+  {
+      name: "The Week",
+      title: "Media Partner",
+      logo: "img/spons/the-week.png"
+  },
+  {
+      name: "DEARC",
+      title: "Media Partner",
+      logo: "img/spons/dearc.png"
+  },
+];
+
+for (var s = 0; s < spons.length; s++) {
   var add = "";
 	add +=
 	'<div class = "person">\
@@ -120,6 +143,14 @@ for (var s in spons) {
 	</div>';
   sponsors_wrapper.innerHTML += add;
 }
+
+var med = "<div id=\"media\">";
+for (var m = 0; m < medias.length; m++) {
+  med += "<div class=\"m\" style=\"background-image:url("+medias[m].logo+")\"></div>";
+}
+med += "</div><div class=\"med person-mail\">Media Partners</div>";
+
+sponsors_wrapper.innerHTML += med;
 
 for(i=0;i<8;i++){
 	if(i%2 == 0){
